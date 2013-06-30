90ac17f079049e7280a5102e1ce602d93b200ede
MrHuxu <hxtheone@gmail.com>
2013-06-30 11:29:39 +0800

    navi-bar is close to finish

--- myjs.js
+++ myjs.js
@@ -1,32 +1,59 @@
 //导航栏
 $(document).ready(function(){$('#mark').css({left:'258px'})})
 $(document).ready(function(){
-	$('#navi1>div').eq(0).mouseover(function(){
+	$('#navi1>div').eq(0).hover(function(){
 		$('#mark').animate({left:'258px'},'fast')
 		$('#link1a').slideDown('slow')
-	}).mouseout(function(){
-	$('#link1a').slideUp('slow')
-	}).end().eq(1).mouseover(function(){
+	},function(){
+		$('#link1a').slideUp('slow')
+	}).end().eq(1).hover(function(){
 		$('#mark').animate({left:'458px'},'fast')
 		$('#link1b').slideDown('slow')
-	}).mouseout(function(){
+	},function(){
 		$('#mark').animate({left:'258px'}, 'slow')
 		$('#link1b').slideUp('slow')
-	}).end().eq(2).mouseover(function(){
+	}).end().eq(2).hover(function(){
 		$('#mark').animate({left:'658px'}, 'fast')
 		$('#link1c').slideDown('slow')
-	}).mouseout(function(){
+	},function(){
 		$('#mark').animate({left:'258px'},'slow')
 		$('#link1c').slideUp('slow')
-	}).end().eq(3).mouseover(function(){
+	}).end().eq(3).hover(function(){
 		$('#mark').animate({left:'858px'}, 'fast')
 		$('#link1d').slideDown('slow')
-	}).mouseout(function(){
+	},function(){
 		$('#mark').animate({left:'258px'},'slow')
 		$('#link1d').slideUp('slow')
 	}).end()
 })
 
+//导航栏2
+$(document).ready(function(){
+	$('#opt2a').hover(function(){
+		$('#opt2a-content').slideDown()
+		$('#mark').animate({marginLeft:'0px'})
+	},function(){
+		$('#opt2a-content').slideUp()
+	})
+	$('#opt2b').hover(function(){
+		$('#opt2b-content').slideDown()
+		$('#mark').animate({marginLeft:'200px'})
+	},function(){
+		$('#opt2b-content').slideUp()
+	})
+	$('#opt2c').hover(function(){
+		$('#opt2c-content').slideDown()
+		$('#mark').animate({marginLeft:'400px'})
+	},function(){
+		$('#opt2c-content').slideUp()
+	})
+	$('#opt2d').hover(function(){
+		$('#opt2d-content').slideDown()
+		$('#mark').animate({marginLeft:'600px'})
+	},function(){
+		$('#opt2d-content').slideUp()
+	})
+})
 //渐隐式的图片轮播
 $(document).ready(function(){
 	$("#pics1").children("img").each(function(){
@@ -71,22 +98,22 @@ $(document).ready(function(){
 	})
 })
 
-//左右滑动式的图片轮播
-$(document).ready(function(){$('#pics2').css({left:'200px'})})
+//左右滑动式的图片轮播F
+$(document).ready(function(){$('#pics2').css({left:'-90px'})})
 var turn2 = 0;
 function slidepic(){
 	switch(turn2 % 4){
 		case 0:{
-			$('#pics2').animate({left:'-500px'},'slow');
+			$('#pics2').animate({left:'-800px'},'slow');
 			break;
 		}case 1:{
-			$('#pics2').animate({left:'-1200px'},'slow');
+			$('#pics2').animate({left:'-1500px'},'slow');
 			break;
 		}case 2:{
-			$('#pics2').animate({left:'-1900px'},'slow');
+			$('#pics2').animate({left:'-2210px'},'slow');
 			break;
 		}case 3:{
-			$('#pics2').animate({left:'200px'},'slow');
+			$('#pics2').animate({left:'-90px'},'slow');
 			break;
 		}default:
 			break;
@@ -101,20 +128,20 @@ $(document).ready(function(){
 		$(this).animate({backgroundColor:'#923'})
 	}).click(function(){
 		switch($('#pics2').css('left')){
-			case '200px':{
-				$('#pics2').animate({left:'-1900px'});
+			case '-90px':{
+				$('#pics2').animate({left:'-2210px'});
 				turn2 = 3;
 				break;
-			}case '-500px':{
-				$('#pics2').animate({left:'200px'});
+			}case '-800px':{
+				$('#pics2').animate({left:'-90px'});
 				turn2 = 0;
 				break;
-			}case '-1200px':{
-				$('#pics2').animate({left:'-500px'});
+			}case '-1500px':{
+				$('#pics2').animate({left:'-800px'});
 				turn2 = 1;
 				break;
-			}case '-1900px':{
-				$('#pics2').animate({left:'-1200px'});
+			}case '-2210px':{
+				$('#pics2').animate({left:'-1500px'});
 				turn2 = 2;
 				break;
 			}
@@ -126,20 +153,20 @@ $(document).ready(function(){
 		$(this).animate({backgroundColor:'#923'})
 	}).click(function(){
 		switch($('#pics2').css('left')){
-			case '200px':{
-				$('#pics2').animate({left:'-500px'});
+			case '-90px':{
+				$('#pics2').animate({left:'-800px'});
 				turn2 = 1;
 				break;
-			}case '-500px':{
-				$('#pics2').animate({left:'-1200px'});
+			}case '-800px':{
+				$('#pics2').animate({left:'-1500px'});
 				turn2 = 2;
 				break;
-			}case '-1200px':{
-				$('#pics2').animate({left:'-1900px'});
+			}case '-1500px':{
+				$('#pics2').animate({left:'-2210px'});
 				turn2 = 3;
 				break;
-			}case '-1900px':{
-				$('#pics2').animate({left:'200px'});
+			}case '-2210px':{
+				$('#pics2').animate({left:'-90px'});
 				turn2 = 0;
 				break;
 			}
